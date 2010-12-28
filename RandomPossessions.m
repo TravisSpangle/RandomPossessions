f9f0fb74f02b2f5acf53c994529f51c691e3827e
Travis Spangle <tspangle@Travis-Spangles-MacBook.local>
2010-12-28 09:10:20 -0800

    Initial creation of NSMutableArray

--- RandomPossessions.m
+++ RandomPossessions.m
@@ -3,8 +3,16 @@
 int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
-    // insert code here...
-    NSLog(@"Hello, World!");
+	NSMutableArray *items = [[[NSMutableArray alloc] init] autorelease];
+	[items addObject:@"one"];
+	[items addObject:@"two"];
+	[items addObject:@"three"];
+	[items insertObject:@"Zero" atIndex:0];
+	
+	for(int i = 0; i < [items count]; i++){
+		NSLog(@"%@", [items objectAtIndex:i]);
+	}
+	
     [pool drain];
     return 0;
 }

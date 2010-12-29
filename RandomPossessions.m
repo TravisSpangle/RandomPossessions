f942d6239f62c949d0c45def93eb9b13f5c377a7
Travis Spangle <tspangle@Travis-Spangles-MacBook.local>
2010-12-28 21:20:48 -0800

    Removed code that populated items array as Possession class random functionality is working

--- RandomPossessions.m
+++ RandomPossessions.m
@@ -6,10 +6,6 @@ int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
 	NSMutableArray *items = [[[NSMutableArray alloc] init] autorelease];
-	[items addObject:@"one"];
-	[items addObject:@"two"];
-	[items addObject:@"three"];
-	[items insertObject:@"Zero" atIndex:0];
 	
 	for(int i = 0; i < 10; i++){
 		[items addObject:[Possession randomPossession]];

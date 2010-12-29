e1a9f478ba2dce9e3983528d3b35c0ba7e520dcb
Travis Spangle <tspangle@Travis-Spangles-MacBook.local>
2010-12-28 18:32:08 -0800

    Added Possession class with properties

---
+++ Possession.h
@@ -0,0 +1,23 @@
+//
+//  Possession.h
+//  RandomPossessions
+//
+//  Created by Travis Spangle on 12/28/10.
+//  Copyright 2010 Peak Systems. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Possession : NSObject {
+	NSString *possessionName;
+	NSString *serialNumber;
+	int valueInDollars;
+	NSDate *dateCreated;
+}
+@property (nonatomic, copy) NSString *possessionName;
+@property (nonatomic, copy) NSString *serialNumber;
+@property (nonatomic) int valueInDollars;
+@property (nonatomic, readonly) NSDate *dateCreated;
+
+@end
---
+++ Possession.m
@@ -0,0 +1,14 @@
+//
+//  Possession.m
+//  RandomPossessions
+//
+//  Created by Travis Spangle on 12/28/10.
+//  Copyright 2010 Peak Systems. All rights reserved.
+//
+
+#import "Possession.h"
+
+@implementation Possession
+@synthesize possessionName, serialNumber, valueInDollars, dateCreated;
+
+@end
